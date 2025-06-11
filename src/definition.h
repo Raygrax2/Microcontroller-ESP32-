b9eb70c031e81bbf6fadcd1db47fdef22bd902e1
Santiago Montoya <raygrax2@gmail.com>
2025-06-11 15:31:50 -0600

    It has another changes to the pins to make a better controll of the pins in the definition.h file

--- src/definition.h
+++ src/definition.h
@@ -10,18 +10,25 @@
 #include <stdio.h>
 #include <SimpleUART.h>
 
-
 uint64_t current_time, previous_time;
 
-const uint8_t echo = 19;   // GPIO pin for echo
-const uint8_t trig = 18;   // GPIO pin for trigger
+const uint8_t echo = 19; // GPIO pin for echo
+const uint8_t trig = 18; // GPIO pin for trigger
 const uint8_t ch_trig = 0;
-
+const uint8_t LCellPin = 34; // Channel for echo
+const uint8_t PIN_MOTOR_A_IN1 = 17;
+const uint8_t PIN_MOTOR_A_IN2 = 5;
+const uint8_t PIN_MOTOR_B_IN1 = 21;
+const uint8_t PIN_MOTOR_B_IN2 = 22;
+const uint8_t PIN_PWM_A = 18;
+const uint8_t PIN_STBY = 16;
+const uint8_t PIN_LOAD_CELL = 34;
+const uint8_t PIN_LED_Green = 4;
+const uint8_t PIN_LED_RED = 15;
 TimerConfig US_config{
     .timer = LEDC_TIMER_3,
     .frequency = 40,
     .bit_resolution = LEDC_TIMER_14_BIT,
-    .mode = LEDC_LOW_SPEED_MODE
-};
+    .mode = LEDC_LOW_SPEED_MODE};
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,6 @@
 #include <definition.h>
-void lcdPrint(SimpleLCD &lcdDisplay, const char *str)
+typedef enum
 {
-    lcdDisplay.writeCommand(CMD_CLEAR);
-    lcdDisplay.writeCommand(CMD_HOME);
-    while (*str)
-    {
-        if (*str == '\n')
-        {
-            lcdDisplay.writeCommand(CMD_SECOND_LINE);
-            str++;
-        }
-        else
-            lcdDisplay.print(*str++);
-    }
-}
-typedef enum {
     STATE_IDLE,
     STATE_MOVING_FORWARD,
     STATE_TURNING_LEFT,
@@ -22,118 +8,115 @@ typedef enum {
     STATE_STOPPED,
     STATE_COLLISION_DETECTED
 } AGVState;
+ 
 extern "C" void app_main()
 {
     esp_task_wdt_deinit();
     Ultrasonic usonic;
     SimpleGPIO motorA_ini1, motorA_ini2, MotorB_ini1, MotorB_ini2, pwmA, stby;
     SimplePWM pwm;
-    SimpleGPIO motorA_ini2; // Declare motorA_ini2
     SimpleADC LCell;
 
-    //setups
+    // setups
     usonic.setup(echo, trig, ch_trig, US_config);
-    
 
-    //Load cell for weight measurement
-    LCell.setup(34); // Setup load cell on GPIO 34
+    // Load cell for weight measurement
+    LCell.setup(LCellPin); // Setup load cell on GPIO 34
     printf("Load cell setup complete.\n");
-    
-    pwmA.setup(18, GPO);
-    motorA_ini2.setup(5, GPO);
-    motorA_ini1.setup(17, GPO);
-    MotorB_ini1.setup(21, GPO);
-    MotorB_ini2.setup(22, GPO);
-    stby.setup(16, GPO);
+
+    pwm.setup(PIN_PWM_A, GPO, &US_config, false); // Setup PWM on pin 18
+    motorA_ini1.setup(PIN_MOTOR_A_IN1, GPO);
+    motorA_ini2.setup(PIN_MOTOR_A_IN2, GPO);
+    MotorB_ini1.setup(PIN_MOTOR_B_IN1, GPO);
+    MotorB_ini2.setup(PIN_MOTOR_B_IN2, GPO);
+    stby.setup(PIN_STBY, GPO);
     stby.set(1); // Enable the motor driver
-    AGVState state = STATE_IDLE; // Initialize state
+    // AGVState state = STATE_IDLE; // Initialize state
     current_time = esp_timer_get_time();
-    motorA_ini1.set(1); // Set motor 1 to high
-    motorA_ini2.set(0); // Set motor 2 to high
-    MotorB_ini1.set(1); // Set motor 3 to high
-    MotorB_ini2.set(0); // Set motor 4 to high
+    motorA_ini1.set(1);                  // Set motor 1 to high
+    motorA_ini2.set(0);                  // Set motor 2 to high
+    MotorB_ini1.set(1);                  // Set motor 3 to high
+    MotorB_ini2.set(0);                  // Set motor 4 to high
     pwm.setup(18, 0, &US_config, false); // Setup PWM on pin 18
-    pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
+    pwm.setDigitalLevel(1000);           // Set PWM duty cycle to 1000 (out of 4095)
     while (1)
     {
-            if (current_time - previous_time >= 1000000)
-            { // 1 second
-                previous_time = current_time;
-                
-                float WEight =LCell.read(ADC_READ_RAW);
-                printf("Load cell reading: %d\n",WEight);
-                /*// --- Collision detection logic ---
-                float dist = usonic.getDistance() / 10;
-                if (dist > 40)
-                {
-                    // Not valid distance, it will be ignored
-                    fprintf(stdout, "Distance is too far, ignoring...\n");
-                }
-                else if (dist < 20)
-                {
-                    printf("Collision imminent!\n");
-                    state = STATE_COLLISION_DETECTED;
-                }
-                else
-                {
-                    fprintf(stdout, "Distance: %.2f cm\n", dist);
-                }
-                // --- End collision detection logic ---
-    
-                switch (state)
-                {
-                case STATE_IDLE:
-                    
-                    break;
-                case STATE_MOVING_FORWARD:
-                    // Move forward
-                    motorA_ini1.set(1); // Set motor 1 to high
-                    motorA_ini2.set(0); // Set motor 2 to high
-                    MotorB_ini1.set(1); // Set motor 3 to high
-                    MotorB_ini2.set(0); // Set motor 4 to high
-                    pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
-                    break;
-                case STATE_TURNING_LEFT:
-                    // Considering the motor a is on the left side and motor b is on the right side
-                    motorA_ini1.set(1); // Set motor 1 to high
-                    motorA_ini2.set(0); // Set motor 2 to high
-                    MotorB_ini1.set(0); // Set motor 3 to high
-                    MotorB_ini2.set(0); // Set motor 4 to high
-                    pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
-                    break;
-                case STATE_TURNING_RIGHT:
-                // Considering the motor a is on the left side and motor b is on the right side
-                    motorA_ini1.set(0); // Set motor 1 to high
-                    motorA_ini2.set(0); // Set motor 2 to high
-                    MotorB_ini1.set(1); // Set motor 3 to high
-                    MotorB_ini2.set(0); // Set motor 4 to high
-                    pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
-                    break;
-                case STATE_STOPPED:
-                    // Stop both motors
-                    motorA_ini1.set(0); // Set motor 1 to high
-                    motorA_ini2.set(0); // Set motor 2 to high
-                    MotorB_ini1.set(0); // Set motor 3 to high
-                    MotorB_ini2.set(0); // Set motor 4 to high
-                    pwm.setDigitalLevel(0); // Set PWM duty cycle to 0 (stop)
-                    // if the ultrasonic sensor changes to not detect a collision, change state to moving forward
-                    if (dist > 20) {
-                        state = STATE_MOVING_FORWARD; // Change state to moving forward
-                    }
-                    break;
-                case STATE_COLLISION_DETECTED:
-                    state = STATE_STOPPED; // Change state to stopped
-                    break;
-                
-                default:
-                    break;
-                }
-                    
-
+        float WEight = LCell.read(ADC_READ_RAW);
+        printf("Load cell reading: %.2f \n", WEight);
+        if (current_time - previous_time >= 1000000)
+        { // 1 second
+            previous_time = current_time;
+            printf("Entro al if \n");
+            /*// --- Collision detection logic ---
+            float dist = usonic.getDistance() / 10;
+            if (dist > 40)
+            {
+                // Not valid distance, it will be ignored
+                fprintf(stdout, "Distance is too far, ignoring...\n");
             }
-                */
-               
-    
-    }
+            else if (dist < 20)
+            {
+                printf("Collision imminent!\n");
+                state = STATE_COLLISION_DETECTED;
+            }
+            else
+            {
+                fprintf(stdout, "Distance: %.2f cm\n", dist);
+            }
+            // --- End collision detection logic ---
 
+            switch (state)
+            {
+            case STATE_IDLE:
+
+                break;
+            case STATE_MOVING_FORWARD:
+                // Move forward
+                motorA_ini1.set(1); // Set motor 1 to high
+                motorA_ini2.set(0); // Set motor 2 to high
+                MotorB_ini1.set(1); // Set motor 3 to high
+                MotorB_ini2.set(0); // Set motor 4 to high
+                pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
+                break;
+            case STATE_TURNING_LEFT:
+                // Considering the motor a is on the left side and motor b is on the right side
+                motorA_ini1.set(1); // Set motor 1 to high
+                motorA_ini2.set(0); // Set motor 2 to high
+                MotorB_ini1.set(0); // Set motor 3 to high
+                MotorB_ini2.set(0); // Set motor 4 to high
+                pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
+                break;
+            case STATE_TURNING_RIGHT:
+            // Considering the motor a is on the left side and motor b is on the right side
+                motorA_ini1.set(0); // Set motor 1 to high
+                motorA_ini2.set(0); // Set motor 2 to high
+                MotorB_ini1.set(1); // Set motor 3 to high
+                MotorB_ini2.set(0); // Set motor 4 to high
+                pwm.setDigitalLevel(1000); // Set PWM duty cycle to 1000 (out of 4095)
+                break;
+            case STATE_STOPPED:
+                // Stop both motors
+                motorA_ini1.set(0); // Set motor 1 to high
+                motorA_ini2.set(0); // Set motor 2 to high
+                MotorB_ini1.set(0); // Set motor 3 to high
+                MotorB_ini2.set(0); // Set motor 4 to high
+                pwm.setDigitalLevel(0); // Set PWM duty cycle to 0 (stop)
+                // if the ultrasonic sensor changes to not detect a collision, change state to moving forward
+                if (dist > 20) {
+                    state = STATE_MOVING_FORWARD; // Change state to moving forward
+                }
+                break;
+            case STATE_COLLISION_DETECTED:
+                state = STATE_STOPPED; // Change state to stopped
+                break;
+
+            default:
+                break;
+            }
+
+
+        }
+            */
+        }
+    }
 }
